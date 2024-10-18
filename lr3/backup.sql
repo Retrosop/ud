0cacca641655dbb64587d946f18f57e4f3d8c352
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-10-18 14:39:09 +1000

    lr3 - Добавлена тестовая функцмя в хранимой процедуре

--- lr3/backup.sql
+++ lr3/backup.sql
@@ -7,3 +7,19 @@ BEGIN
   SELECT('Hello, world!');
 END
 //
+
+CALL glagolev_Hello_World();
+
+
+/*
+	Тестовый запросов на создание хранимой процедуры
+*/
+DELIMITER //
+CREATE FUNCTION glagolev_Hello_World2()
+RETURNS VARCHAR(20)
+BEGIN
+  RETURN 'Hello, world!';
+END
+//
+
+SELECT glagolev_Hello_World2();
