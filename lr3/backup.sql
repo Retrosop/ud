b54a60daf7d868b7a7fd7085cdafbb995c6e84c6
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-11-01 13:55:51 +1000

    Добавлен запрос на курсор в динамическом запросе

--- lr3/backup.sql
+++ lr3/backup.sql
@@ -68,3 +68,26 @@ END
 SET @s=0;
 CALL glagolev_Parts_count_1('Сталь',@s);
 SELECT @s;
+
+DELIMITER //
+CREATE PROCEDURE glagolev_Parts_rename(PName VARCHAR(20))
+BEGIN
+  DECLARE Done INT DEFAULT 0;
+  DECLARE S VARCHAR(20);
+  DECLARE N,I INTEGER;
+  DECLARE Cur1 CURSOR FOR SELECT Part_ID, Part_name FROM Parts WHERE Part_name=PName ORDER BY WEIGHT;
+  DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done = 1;
+  OPEN Cur1;
+  SET I=1;
+  REPEAT
+    FETCH Cur1 INTO N,S;
+    IF Done=0 THEN UPDATE Parts SET Part_name=CONCAT(S,'-',I) WHERE Part_ID=N;
+    END IF;
+  SET I=I+1;
+  UNTIL Done END REPEAT;
+  CLOSE Cur1;
+END
+
+//
+
+CALL glagolev_Parts_rename('Car');
